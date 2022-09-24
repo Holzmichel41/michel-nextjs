d5bbaf98d1a54219bdca68ce2f1f731e0c5709af
holzmichel41 <47560723+Holzmichel41@users.noreply.github.com>
2022-09-24 21:47:07 +0200

    feat: say hello to the world

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,17 +14,17 @@ const Home: NextPage = () => {
       </Head>
 
       <Canvas />
-      {/* <main className={styles.main}>
+      <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">my space!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.tsx</code>
+          My name is &nbsp; <code className={styles.code}>{"<Michel />"}</code>{" "}
+          &nbsp; and I&apos;m a frontend engineer
         </p>
 
-        <div className={styles.grid}>
+        {/* <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
@@ -52,10 +52,10 @@ const Home: NextPage = () => {
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
           </a>
-        </div>
-      </main> */}
+        </div> */}
+      </main>
 
-      {/* <footer className={styles.footer}>
+      <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
@@ -66,7 +66,7 @@ const Home: NextPage = () => {
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
-      </footer> */}
+      </footer>
     </div>
   );
 };
